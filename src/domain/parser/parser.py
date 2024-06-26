18158916b1bf0d465ff51cc2b54be0d1bc8efb19
Александр Рахмаев <rahmaevao@gmail.com>
2024-06-26 18:52:26 +0300

    - Лишние логи

--- src/domain/parser/parser.py
+++ src/domain/parser/parser.py
@@ -14,13 +14,10 @@ class DslParser:
         return DslModule(body=self.__further_parse_one_layer(raw_parsing))
 
     def __further_parse_one_layer(self, raw_parsing: ParseResults) -> list[DslNode]:
-        logger.debug(f"Parsing one layer {raw_parsing}\n{raw_parsing.as_dict()}")
         nodes = []
         children = []
         if "children" in raw_parsing:
-            logger.debug(f"Fined children for `{raw_parsing.get("entity_id")}`")
             children = self.__parse_children(raw_parsing.get("children"))
-        logger.debug(f"End of parsing {raw_parsing}. Start create node")
         nodes.append(
             DslNode(
                 id=raw_parsing["entity_id"],
@@ -33,7 +30,6 @@ class DslParser:
     def __parse_children(
         self, raw_children: ParseResults
     ) -> list[DslProperty | DslNode]:
-        logger.debug(f"Parsing children {raw_children}")
         children = []
         child_part = raw_children[0]
         for child in child_part:
