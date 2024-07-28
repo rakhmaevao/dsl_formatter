522d1ebcb4836f74f8e79989998b9dd632495211
Александр Рахмаев <rahmaevao@gmail.com>
2024-07-28 23:42:53 +0300

    риукрасил

--- src/presentation/stdout_reporter.py
+++ src/presentation/stdout_reporter.py
@@ -3,5 +3,6 @@ from src.domain.checkers import CheckResult
 
 class StdoutReporter:
     def report(self, results: list[CheckResult]) -> None:
+        print(f"Found {len(results)} problems:")
         for result in results:
             print(result)
